2b83513b406ad7a50466abdae9dce5337969a9f5
omarfarids <omar.farid007@gmail.com>
2024-01-18 12:47:26 +0200

    login

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,60 +1,48 @@
-import React, { useState } from "react";
-// import { useField, ErrorMessage } from 'vee-validate';
+import React, { forwardRef, useState } from "react";
 import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
 
 interface TextInputProps {
-  placeholder: string;
+  placeholder?: string;
   type?: "text" | "password";
-  name: string;
-  onChange: any;
-  value: string;
 }
 
-const TextInput = ({
-  placeholder,
-  type = "text",
-  name,
-  onChange,
-  value,
-}: TextInputProps) => {
-  //   const { value, errorMessage, handleInput } = useField(name, isRequired);
-  const [isPassword, setIsPassword] = useState(true);
+const TextInput = forwardRef(
+  ({ placeholder, type = "text", ...props }: TextInputProps, ref: any) => {
+    const [isPassword, setIsPassword] = useState(true);
 
-  const togglePassword = () => {
-    setIsPassword(!isPassword);
-  };
-
-  return (
-    <div>
-      <div className="relative">
-        <input
-          type={isPassword ? type : "text"}
-          name={name}
-          placeholder={placeholder}
-          className="input input-bordered input-md w-full sm:w-[450px] rounded-sm"
-          value={value}
-          onChange={onChange}
-        />
-        {type === "password" && (
-          <div className="absolute right-3 top-[13px] cursor-pointer">
-            {isPassword ? (
-              <VisibilityOutlinedIcon
-                fontSize="small"
-                onClick={togglePassword}
-              />
-            ) : (
-              <VisibilityOffOutlinedIcon
-                fontSize="small"
-                onClick={togglePassword}
-              />
-            )}
-          </div>
-        )}
+    const togglePassword = () => {
+      setIsPassword(!isPassword);
+    };
+    return (
+      <div>
+        <div className="relative">
+          <input
+            type={isPassword ? type : "text"}
+            placeholder={placeholder}
+            className="input input-bordered input-md w-full sm:w-[450px] rounded-sm"
+            {...props}
+            ref={ref}
+          />
+          {type === "password" && (
+            <div className="absolute right-3 top-[13px] cursor-pointer">
+              {isPassword ? (
+                <VisibilityOutlinedIcon
+                  fontSize="small"
+                  onClick={togglePassword}
+                />
+              ) : (
+                <VisibilityOffOutlinedIcon
+                  fontSize="small"
+                  onClick={togglePassword}
+                />
+              )}
+            </div>
+          )}
+        </div>
       </div>
-      {/* <ErrorMessage name={name} className="text-red-500 text-sm p-2" /> */}
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default TextInput;
--- src/modules/auth/pages/Login.tsx
+++ src/modules/auth/pages/Login.tsx
@@ -1,48 +1,48 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import TextInput from "@/components/TextInput"; // Adjust the import path as needed
 import Button from "@/components/Button"; // Adjust the import path as needed
+import { useForm, SubmitHandler } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const Login = () => {
-  const navigate = useNavigate();
-
-  const [userData, setUserData] = useState({
-    userName: "",
-    password: "",
+  // ------------ hooks -------------
+  const schema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().required(),
   });
 
-  const validateUserData = () => {
-    return userData.userName.length > 0 && userData.password.length > 0;
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  }: any = useForm({
+    resolver: yupResolver(schema),
+  });
 
-  const handleLogin = (e: any) => {
-    if (validateUserData()) {
-      e.preventDefault();
-      navigate("/home");
-    }
+  const navigate = useNavigate();
+
+  // ------------ functions ------------
+  const onSubmit: SubmitHandler<any> = () => {
+    navigate("/home");
   };
 
   return (
     <section>
-      <form onSubmit={handleLogin} className="flex flex-col gap-2 items-center">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-2 items-center"
+      >
         <h1 className="text-3xl font-semibold mb-5">Login to your account</h1>
+        <TextInput placeholder="Email Address" {...register("email")} />
+        <p>{errors.email?.message}</p>
         <TextInput
-          name="email"
-          value={userData.userName}
-          placeholder="Email Address"
-          onChange={(e: any) =>
-            setUserData({ ...userData, userName: e.target.value })
-          }
-        />
-        <TextInput
-          name="password"
           placeholder="Password"
           type="password"
-          value={userData.password}
-          onChange={(e: any) =>
-            setUserData({ ...userData, password: e.target.value })
-          }
+          {...register("password")}
         />
+        <p>{errors.password?.message}</p>
         <Button label="Login" className="w-full rounded-sm mt-2" />
       </form>
     </section>
