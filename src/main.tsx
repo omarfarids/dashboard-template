53b0d6e56a2c8168bf75c6a56799270eea2a4ec4
omarfarids <omar.farid007@gmail.com>
2024-02-07 16:48:30 +0200

    notification and loader

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import { store } from "@/store/store";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <React.StrictMode>
       <Provider store={store}>
+        <ToastContainer />
         <App />
       </Provider>
     </React.StrictMode>
