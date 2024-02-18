ec668d452ef5cffb0a5ff437078842b4e42bb738
mido <midosaedsef@gmail.com>
2024-02-18 11:41:05 +0200

    Ui update

--- src/modules/customer/pages/Products.tsx
+++ src/modules/customer/pages/Products.tsx
@@ -19,38 +19,23 @@ const Products = () => {
   const [itemIDs, setItemIDs] = useState<any>([]);
 
   return (
-    <div>
-      <div className="">
-        <button
-          className="btn btn-neutral w-fit"
-          onClick={() => navigate(`/customer/orders`)}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-            />
-          </svg>
-        </button>
-      </div>
+    <div
+      style={{
+        backgroundColor: "#0b7180",
+        height: "100%",
+      }}
+    >
+      <div className="">s</div>
 
-      <div className="min-h-96 mt-10">
+      <div className="min-h-96 rounded-xl bg-white mx-12 mt-16">
         {isLoading ? (
           <Loading />
         ) : (
-          <div className="flex flex-row flex-wrap justify-start gap-5 m-5  ">
+          <div className="flex flex-row flex-wrap justify-start gap-5 p-5 ">
             {data?.data?.map((item: any) => (
               <div
                 key={item?._id}
-                className="card card-compact w-80 bg-base-100 shadow-md hover:shadow-2xl cursor-pointer border border-gray"
+                className="card card-compact w-80 bg-base-100 shadow-md hover:shadow-2xl cursor-pointer border border-gray  hover:bg-cardBg hover:text-white"
               >
                 <div>
                   <figure>
